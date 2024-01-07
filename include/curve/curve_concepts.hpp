4ff3ca095db3e858d07e9b2093de0647eb5f28f8
k.konishi <53860933+Konishi2911@users.noreply.github.com>
2024-01-07 17:40:04 +0900

    [ADD] add a class representing a segment

---
+++ include/curve/curve_concepts.hpp
@@ -0,0 +1,23 @@
+#pragma once 
+#include <type_traits>
+#include <concepts>
+
+namespace geomlib {
+
+template<typename T>
+concept Curve = requires(const T& t) {
+    typename T::PointType;
+    typename T::VectorType;
+
+    // Query to get points on the curve at the given normalized position.
+    { t(std::declval<double>()) } -> std::convertible_to<typename T::PointType>;
+    { t.point(std::declval<double>()) } -> std::convertible_to<typename T::PointType>;
+
+    // Query to get the length of the curve.
+    { t.length() } -> std::convertible_to<double>;
+
+    // Query to get the normalized tangent vector at the specified position.
+    { t.tangent(std::declval<double>()) } -> std::convertible_to<typename T::VectorType>;
+};
+
+}
