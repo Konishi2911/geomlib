a36a52238d1418a49ea07c63c09a162dc31e9ef3
k.konishi <53860933+Konishi2911@users.noreply.github.com>
2024-01-21 19:30:14 +0900

    [ADD] add an overload of `sample` that allows sampling feature for the curves not conforming to `LengthMeasurableCurve`

--- include/curve.hpp
+++ include/curve.hpp
@@ -11,6 +11,13 @@
 
 namespace geomlib {
 
+/// @brief  Returns the approximate length of the given curve.
+/// @tparam C 
+/// @param ss start point [0.0, 1.0]
+/// @param es end point [0.0, 1.0], `ss < es`
+/// @param curve 
+/// @param n 
+/// @return 
 template<Curve C>
 auto approx_length(double ss, double es, const C& curve, size_t n) -> double {
     auto integrator = mathlib::integral::Simpson();
@@ -37,6 +44,34 @@ auto sample(const C& curve, size_t n, F&& f) noexcept -> geomlib::Polyline<Dim<C
         auto s = static_cast<double>(k) / (n - 1);
         auto len = tot_len * f(s);
         auto t = root_solver.find_root([&](double x){ return len - curve.length(x); }, 0.0, 1.0).value();
+
+        vec.emplace_back(curve.point(t));
+    }
+    auto polyline = Polyline<N>(std::move(vec));
+    return polyline;
+}
+
+/// @brief Creates a polyline object by sampling the point on the given curve with specified spacing function.
+/// @tparam F   the type of spacing function 
+/// @tparam C   the curve type that conforms to `LengthMeasurableCurve`
+/// @param curve    a curve sampling the point 
+/// @param n        the number of sampling point including endpoints. 
+/// @param f        the spacing function invocable with double-type parameter that range is [0, 1]. 
+/// @return 
+template<Curve C, std::invocable<double> F>
+auto sample(const C& curve, size_t n, F&& f) noexcept -> geomlib::Polyline<Dim<C>()> {
+    constexpr size_t N = Dim<C>();
+
+    constexpr size_t n_len = 100;
+    auto root_solver = mathlib::Secant(1e-8);
+    auto tot_len = approx_length(0.0, 1.0, curve, n_len);
+
+    auto vec = std::vector<lalib::VecD<N>>();
+    vec.reserve(n);
+    for (auto k = 0u; k < n; ++k) {
+        auto s = static_cast<double>(k) / (n - 1);
+        auto len = tot_len * f(s);
+        auto t = root_solver.find_root([&](double x){ return len - approx_length(0.0, x, curve, n_len); }, 0.0, 1.0).value();
 
         vec.emplace_back(curve.point(t));
     }
