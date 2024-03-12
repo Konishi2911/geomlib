140d81ea6ac93ae5fc3e6f40e39e9cd01cfa306e
k.konishi <53860933+Konishi2911@users.noreply.github.com>
2024-03-12 16:17:19 +0900

    [ADD] add PolygonView

--- include/geomlib/surface/polygon.hpp
+++ include/geomlib/surface/polygon.hpp
@@ -5,6 +5,7 @@
 
 #include "lalib/vec.hpp"
 #include "lalib/mat.hpp"
+#include <functional>
 
 namespace geomlib {
 
@@ -29,6 +30,32 @@ private:
     static auto __calc_base(const std::vector<PointType>& verts) noexcept -> std::array<lalib::VecD<3>, 2>;
 };
 
+
+struct PolygonView {
+    using PointType = lalib::VecD<3>;
+    using VectorType = lalib::VecD<3>;
+
+    PolygonView(std::vector<std::reference_wrapper<const PointType>>&& verts) noexcept;
+
+    auto point(const lalib::VecD<2>& s) const -> PointType;
+    auto normal(const lalib::VecD<2>& s = lalib::VecD<2>::filled(0.0)) const -> VectorType;
+
+    auto verts() const noexcept -> const std::vector<std::reference_wrapper<const PointType>>&;
+    auto origin() const noexcept -> PointType;
+    auto area() const noexcept -> double;
+    auto base() const noexcept -> lalib::MatD<3, 2>;
+
+private:
+    std::vector<std::reference_wrapper<const PointType>> _verts;
+    std::array<lalib::VecD<3>, 2> _base;
+
+    static auto __calc_base(const std::vector<std::reference_wrapper<const PointType>>& verts) noexcept -> std::array<lalib::VecD<3>, 2>;
+};
+
+
+// =========================== //
+// Polygon                     //
+// =========================== //
 
 inline Polygon::Polygon(std::vector<PointType>&& verts) noexcept: 
     _verts(verts), _base(__calc_base(verts))
@@ -86,7 +113,68 @@ inline auto Polygon::__calc_base(const std::vector<PointType>& verts) noexcept -
     return std::array { u1, u2 };
 }
 
+// =========================== //
+// PolygonView                 //
+// =========================== //
+
+inline PolygonView::PolygonView(std::vector<std::reference_wrapper<const PointType>>&& verts) noexcept: 
+    _verts(verts), _base(__calc_base(verts))
+{}
+
+inline auto PolygonView::verts() const noexcept -> const std::vector<std::reference_wrapper<const PointType>>& {
+    return this->_verts;
+}
+
+inline auto PolygonView::point(const lalib::VecD<2>& s) const -> PointType {
+    auto p = this->origin();
+    lalib::axpy(s[0], this->_base[0], p);
+    lalib::axpy(s[1], this->_base[1], p);
+    return p;
+}
+
+inline auto PolygonView::normal(const lalib::VecD<2>&) const -> PointType {
+    auto n = lalib::cross(this->_base[0], this->_base[1]);
+    return n;
+}
+
+inline auto PolygonView::origin() const noexcept -> PointType {
+    auto o = this->_verts[0];
+    return o;
+}
+
+inline auto PolygonView::area() const noexcept -> double {
+    auto darea = lalib::VecD<3>::filled(0.0);
+    for (auto i = 1u; i < this->_verts.size() - 1; ++i) {
+        auto p1 = this->_verts[i].get() - this->_verts[0].get();
+        auto p2= this->_verts[i + 1].get() - this->_verts[0].get();
+        lalib::cross_acc(p1, p2, darea);
+    }
+    auto area = 0.5 * darea.norm2();
+    return area;
+}
+
+inline auto PolygonView::base() const noexcept -> lalib::MatD<3, 2> {
+    auto base = lalib::MatD<3, 2>({
+        this->_base[0][0], this->_base[1][0],
+        this->_base[0][1], this->_base[1][1],
+        this->_base[0][2], this->_base[1][2]
+    });
+    return base;
+}
+
+
+inline auto PolygonView::__calc_base(const std::vector<std::reference_wrapper<const PointType>>& verts) noexcept -> std::array<lalib::VecD<3>, 2> {
+    auto n = verts.size();
+    auto u1 = verts[1].get() - verts[0].get();
+    auto u2 = verts[n - 1].get() - verts[0].get();
+    u1 = u1 / u1.norm2();
+    u2 = u2 / u2.norm2();
+
+    return std::array { u1, u2 };
+}
+
 static_assert(Surface<Polygon>);
+static_assert(Surface<PolygonView>);
 
 }
 
